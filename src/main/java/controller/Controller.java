4b82d2d10a67a41ca7c33baf9c8caf12b7004248
Kristoffer Baumgarten <s180500@student.dtu.dk>
2019-11-15 10:24:50 +0100

    Gui. add text til hvor mange spiller skal være med

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -9,6 +9,7 @@ public class Controller {
      public void playgame(){
          GUI gui = new GUI(Board.createBoard(), Color.cyan);
 
+         gui.getUserInteger("How many players are you?",2,4);
 
      }
 
