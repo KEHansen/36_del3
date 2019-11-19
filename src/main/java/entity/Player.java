bb439659b7f56898b6fd44a2eaae3c1c49c676ba
Simon <fridolf.lindhard@gmail.com>
2019-11-19 17:05:06 +0100

    Player class and start of Playerlist

--- src/main/java/entity/Player.java
+++ src/main/java/entity/Player.java
@@ -1,22 +1,26 @@
 package entity;
-
-import gui_fields.GUI_Car;
-
 import java.awt.*;
 
 public class Player {
     private String name;
     private Color color;
+    private int money;
+    private int feltIndex;
+    private boolean haveLost;
+    private boolean throwAgain;
+    private boolean inPrison;
+
 
     public Player(String name, Color color) {
-        this.name = name;
-        this.color = color;
-
+        //todo "Husk at argumentere for at vi har money, som en parameter."
+        money=0;
+        feltIndex=0;
+        haveLost=false;
+        throwAgain=false;
+        inPrison=false;
     }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
 
     public void setName(String name) {
         this.name = name;
@@ -30,9 +34,32 @@ public class Player {
         this.color = color;
     }
 
-    @Override
-    public String toString() {
-        return name;
+    public int getMoney() {
+        return money;
     }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public boolean isHaveLost() {return haveLost;}
+
+    public void setHaveLost(boolean haveLost) { this.haveLost = haveLost; }
+
+    public boolean isThrowAgain(){return throwAgain;}
+
+    public void setThrowAgain(boolean throwAgain) { this.throwAgain = throwAgain; }
+
+    public int getFeltIndex() { return feltIndex; }
+
+    public void setFeltIndex(int feltIndex) { this.feltIndex = feltIndex; }
+
+    public boolean isInPrison(){return inPrison;}
+
+    public  boolean setInPrison(boolean inPrison){return haveLost;}
+
+    @Override
+    public String toString() { return name; }
+
 }
 
--- src/main/java/entity/Playerlist.java
+++ src/main/java/entity/Playerlist.java
@@ -1,35 +1,38 @@
 package entity;
-
-
-import java.awt.*;
-
 public class Playerlist {
 
-    private Player[] players;
+    //ide fra læres CDIO 2:
 
-    public Playerlist(int playersNum) {
-        players = new Player[playersNum];
-        for (int i = 0; i < playersNum; i++)
-            players[i] = new Player();
-    }
-
-        void p ( int i, String s){
-            players[i].setName();
-        }
-
-
-    /**
-     * tilf�jer harcoded spilere til spillerlisten
-     * @param antal antal spillere der skal tilf�jes
-     */
-    public void addplayer(int antal){
-        String[] navne = {};
-        for (int i=0; i<antal;i++){
-            players[i].setName(navne[i]);
-            //Players[i].setColor();
-        }
-    }
+//    private Player[] players;
+//
+//    public Playerlist(int playersNum) {
+//        players= new Player[playersNum];
+//        for (int i = 0;i < playersNum;i++)
+//            players[i]=new Player();
+//    }
+//
+//    public void addPlayer (int antal){
+//
+//    }
 
 
+
+
+
+   // Gammelt stuff - formenligt forkert.
+
+// /*       void p ( int i, String s){
+//            players[i].setName();
+//        }
+//
+//    public void addplayer(int antal){
+//        String[] navne = {};
+//        for (int i=0; i<antal;i++){
+//            players[i].setName(navne[i]);
+//            //Players[i].setColor();
+//        }
+//    }
+//
+//*/
 
 }
