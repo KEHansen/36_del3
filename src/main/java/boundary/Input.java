cba5445692892c855608dd5fb30a68c9ac10fc8e
Karl Emil <kehansen3@hotmail.dk>
2019-11-19 15:30:33 +0100

    Nu går det løs.

--- src/main/java/boundary/Input.java
+++ src/main/java/boundary/Input.java
@@ -1,13 +1,15 @@
 package boundary;
-import controller.Controller;
-import entity.Player;
-import gui_fields.GUI_Player;
+
 import gui_main.GUI;
 
 public class Input {
-GUI gui = new GUI();
-    public void addPlayer (Player p) {
-        gui.addPlayer(new GUI_Player(p.getName()));
+
+    private GUI gui;
+
+    public Input(GUI gui) {
+        this.gui = gui;
     }
+
+    public void
 
 }
