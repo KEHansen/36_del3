a41bd58d45520fffa75225577b10515f4db5e81b
malouzeuthen-bruun <malouzb@hotmail.com>
2019-11-26 10:12:38 +0100

    fra dansk til engelsk

--- src/main/java/boundary/Text.java
+++ src/main/java/boundary/Text.java
@@ -3,11 +3,11 @@ package boundary;
 public class Text {
     public final static String[] TEXT =
     {
-                "Venter på spiller %s. Tast ENTER for at fortsætte \n",
-                "Terning 1: %s, Terning 2: %s \n",
+                "Waiting for player %s. Click ENTER to continue \n",
+                "Dice 1: %s, Dice 2: %s \n",
                 "Status: ",
-                "Spiller %s : %d  *** ",
-                "Spiller %s : Landede p� \" The Werewall \". Du m� sl� igen \n",
-                "Spillet er slut. %s vandt"
+                "Player %s : %d  *** ",
+                "Player %s : Landed p� \" The Werewall \". you may strike again \n",
+                "The game is over. %s won"
     };
 }
