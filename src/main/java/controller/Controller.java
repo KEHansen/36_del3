ba4e65fcd7d59534aa9902a42e6188bdba79ab12
Karl Emil <kehansen3@hotmail.dk>
2019-11-27 14:20:31 +0100

    Fix på GameLogic.

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -9,7 +9,7 @@ import entity.Player;
 import entity.PlayerList;
 import gui_main.GUI;
 import logic.ChanceCardLogic;
-import logic.hej2;
+import logic.GameLogic;
 import java.awt.Color;
 
 
@@ -25,7 +25,7 @@ public class Controller {
 
          MatadorGUI matGUI = new MatadorGUI(gui);
 
-         hej2 logic = new hej2();
+         GameLogic logic = new GameLogic();
 
          Dice d1 = new Dice(6);
 
