f0b4b534d8f2b2c63d9890e6403c0ce3fa902000
Kristoffer Baumgarten <s180500@student.dtu.dk>
2019-11-27 15:17:16 +0100

    rent tekst

--- src/main/java/boundary/Text.java
+++ src/main/java/boundary/Text.java
@@ -7,7 +7,8 @@ public class Text {
                     "Set player %d's name \n",
                     "Waiting for %s. Click ENTER to continue \n",
                     "%s has to pay 1M in order to get out of prison. \n",
-                    "%s landed on chance and has to pick a chance card \n",
+                    "%s landed on chance and has to draw a chance card \n",
+                    "%s chose to draw another card \n",
                     "The game is over. %s won \n"
             };
 
@@ -31,10 +32,6 @@ public class Text {
                     "Move to an MAGENTA or a CYAN field if it's available you get it for free, if not paid the owner rent \n", // 15 ChanceCard
                     "you have done all of your homework, you receive 2M\n ", // 16 chanceCard
                     "Move to an RED field if it's available you get it for free, if not paid the owner rent \n", // 17 ChanceCard
-
-
-
-
 };
 
     public final static String[] interactiveTEXT =
@@ -44,14 +41,21 @@ public class Text {
                     "Do you wish to move to (1) SKATER PARK or (2) SWIMMING POOL? \n", // 4 ChanceCard
                     // ---------------------- 5 ChanceCard ------------------------------
                     "Do you wish to (1) move 1 field or (2) draw another \n", // 6 ChanceCard
-                    "Do you wish to move to (1) ORANGE or (2) GREEN? \n", // 8.1 ChanceCard
-                    "you Chose ORANGE, which would you like to move to? (1) SKATER PARK or (2) SWIMMING POOL? \n", // 8.2 ChanceCard
-                    "you Chose GREEN, which would you like to move to? (1) BOWLING ALLEY or (2) ZOO? \n", // 8.3 ChanceCard
-                    "Do you wish to move to (1) CANDY SHOP or (2) IS-CREAM SHOP? \n", // 7 ChanceCard
+
+                    "Do you wish to move to ORANGE or GREEN? \n " +
+                            "ORANGE: (1) SKATER PARK or (2) SWIMMING POOL\n" +
+                            "GREEN:  (1) BOWLING ALLEY or (2) ZOO? \n", // 8.1 ChanceCard
+
+                    "Do you wish to move to (1) CANDY SHOP or (2) IS-CREAM SHOP? \n", // 9 ChanceCard
                     // ---------------------- 12 ChanceCard ------------------------------
                     // ---------------------- 13 ChanceCard ------------------------------
-                    "you Chose MAGENTA, which would you like to move to? (1) MUSEUM or (2) LIBRARY? \n", // 15.2 ChanceCard
-                    "you Chose CYAN, which would you like to move to? (1) WATER PARK or (2) SEAFRONT? \n", // 15.3 ChanceCard
+
+                    "Do you wish to move to MAGENTA or CYAN? \n " +
+                            "ORANGE: (1) MUSEUM or (2) LIBRARY\n" +
+                            "GREEN:  (1) WATER PARK or (2) SEAFRON? \n", // 15.1 Chancecard
+
+                    "Do you wish to move to (1) ARCADE or (2) CINEMA? \n", // 17 ChanceCard
+
 
             };
 }
